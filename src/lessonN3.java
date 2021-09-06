d14626845097986c2cb004e650bdc3dcbfa95554
Vahan922 <musayelyanvahan@gmail.com>
2021-09-06 21:26:52 +0400

    jgjgj

---
+++ src/lessonN3.java
@@ -0,0 +1,38 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class lessonN3 {
+
+       public static void main(String[]args) {
+       Scanner inputScanner = new Scanner(System.in);
+//     System.out.println("enter your number 1");
+//     int a = inputScanner.nextInt();
+//     System.out.println("enter your number 2");
+//     int b = inputScanner.nextInt();
+//    int  c;
+//     c = a+b;
+//     System.out.println("the Sum is ;" + c);
+
+
+//           System.out.println("mutq tveq");
+//           String inputStr1 = inputScanner.next();
+//           String inputStr2 = inputScanner.next();
+//           System.out.println("nor mutq  " + inputStr1 +"  "+ inputStr2);
+
+//           System.out.println("mutq tveq");
+//           String inputStr1 = inputScanner.next();
+//           boolean hasNext = inputScanner.hasNext();
+//           System.out.println("has next arg;" + hasNext);
+//           String inputStr2 = inputScanner.next();
+//           System.out.println("nor mutq  " + inputStr1 +"  "+ inputStr2);
+
+
+if (args.length>0){
+    System.out.println("firdt:"+ args[0]);
+
+}
+else {
+    System.out.println("no arguments");
+}
+ }
+}
